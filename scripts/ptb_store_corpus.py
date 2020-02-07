e25d0e4b90b141375b6a90ee6bfb0a005b66551d
abollu779 <abollu779@gmail.com>
2020-02-07 18:27:18 -0500

    Able to create raw txt files alongside the conllu files

--- scripts/ptb_store_corpus.py
+++ scripts/ptb_store_corpus.py
@@ -5,7 +5,7 @@ from nltk.corpus import treebank
 
 train_startidx, dev_startidx, test_startidx = 0, 150, 180
 
-def write_dataset_to_file(out_file, split='train'):
+def write_dataset_to_file(out_conll_file, out_txt_file, split='train'):
     fileids = []
     if split == 'train':
         fileids = treebank.fileids()[:dev_startidx]
@@ -15,18 +15,35 @@ def write_dataset_to_file(out_file, split='train'):
         assert split == 'test'
         fileids = treebank.fileids()[test_startidx:]
     
+    # Collect corpus data
     tagged_sents = []
+    sents = []
     for filename in fileids:
-        file_data = treebank.tagged_sents(filename) # outputs data from that file in the form [(word1, label1), (word2, label2),....]
-        tagged_sents.extend(file_data)
-        
-    with open(out_file, 'w') as out_fp:
+        file_tagged_data = treebank.tagged_sents(filename) 
+        # outputs data from that file in the form:
+        # [[(word1_sent1, label1_sent1), (word2_sent1, label2_sent1),..],[(word1_sent2,label1_sent2),...],...]
+        tagged_sents.extend(file_tagged_data)
+
+        file_sents = treebank.sents(filename)
+        # outputs data from that file in the form:
+        # [[word1_sent1, word2_sent1,..],[word1_sent2,...],...]
+        sents.extend(file_sents)
+
+    # Write conll file
+    with open(out_conll_file, 'w') as out_conll_fp:
         for tagged_sent in tagged_sents:
             for i, tagged_word in enumerate(tagged_sent):
-                out_fp.write('%d\t%s\t%s\n' % (i, tagged_word[0], tagged_word[1]))
-            out_fp.write('\n')
+                out_conll_fp.write('%d\t%s\t%s\n' % (i, tagged_word[0], tagged_word[1]))
+            out_conll_fp.write('\n')
+
+    # Write raw txt file
+    with open(out_txt_file, 'w') as out_txt_fp:
+        for sent in sents:
+            out_txt_fp.write(' '.join(sent))
+            out_txt_fp.write('\n')
+
 
 if __name__=="__main__":
-    write_dataset_to_file('example/data/ptb_nltk/ptb_train.conllu', 'train')
-    write_dataset_to_file('example/data/ptb_nltk/ptb_dev.conllu', 'dev')
-    write_dataset_to_file('example/data/ptb_nltk/ptb_test.conllu', 'test')
+    write_dataset_to_file('example/data/ptb_nltk/ptb_train.conllu', 'example/data/ptb_nltk/ptb_train.txt', 'train')
+    write_dataset_to_file('example/data/ptb_nltk/ptb_dev.conllu', 'example/data/ptb_nltk/ptb_dev.txt','dev')
+    write_dataset_to_file('example/data/ptb_nltk/ptb_test.conllu', 'example/data/ptb_nltk/ptb_test.txt','test')
